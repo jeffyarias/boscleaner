fd21a434c924f603733af79ccd41120081dab75b
jeffyarias <jeffreyarias21@gmail.com>
2020-03-04 16:36:54 -0500

    Added server and client

--- server.js
+++ server.js
@@ -198,6 +198,20 @@ const customer = await stripe.customers.create({
 
 app.get('/', (req, res) => res.send('API Running'));
 
+if (process.env.NODE_ENV === 'production') {
+   // Express will server up production assests
+   // like our main.js file, or main.css file!
+   app.use(express.static('client/build'));
+
+
+// Express will serve up the index.html file if doesn't reconize the route
+app.get('*', (req, res)=> { 
+res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+
+})
+
+}
+
 const PORT = process.env.PORT || 3001;
 
 
