fc10d327a7e13c52b105278be63d72101f3e3d3a
jeffy arias <jeffyarias@jeffys-MacBook-Pro.local>
2020-04-01 23:31:47 -0400

    after icons

--- client/src/components/extras.js
+++ client/src/components/extras.js
@@ -2,6 +2,8 @@ import React from 'react';
 import extra from './css/Extras.module.css';
 import fridge from '../images/kitchen.png';
 import oven from '../images/gas-stove.png';
+import truck from '../images/moving-truck.png';
+import wiping from '../images/wiping.png'
 const extras = ()=> {
 
 
@@ -9,10 +11,21 @@ const extras = ()=> {
         <div className={extra.extrasIcons}>
        <div className={extra.insidefridge}>
         <img src={fridge} />
+        <p className={extra.title}>Inside the fridge</p>
     
        </div>
        <div className={extra.insidefridge}>
            <img src={oven}/>
+           <p className={extra.title}>Inside the Oven</p>
+       </div>
+       <div className={extra.insidefridge}>
+           <img src={truck }/>
+           <p className={extra.title}>Move in/out</p>
+           </div>
+
+           <div className={extra.insidefridge}>
+           <img src={wiping }/>
+           <p className={extra.title}>Wall Washing</p>
        </div>
         </div>
     )
